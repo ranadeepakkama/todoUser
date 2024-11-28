a9c4fac65bc9333eae9b4d1358ca738e5c048d60
Rana Deepak <61343234+ranadeepakkama@users.noreply.github.com>
2024-11-28 19:04:34 +0530

    updated notfound code

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,10 +1,8 @@
-import Navbar from '../Navbar'
 import './index.css'
 
 
 const NotFound = () => (
         <div>
-            <Navbar/>
             <div className='notfound-container'>
                 <img className='notfound-img' src='https://media.istockphoto.com/id/1404059706/vector/website-page-not-found-error-404-oops-worried-robot-character-peeking-out-of-outer-space.jpg?s=612x612&w=0&k=20&c=DvPAUof9UsNuNqCJy2Z7ZLLk75qDA3bbLXOOW_50wAk=' alt='not-Found'/>
             </div>
